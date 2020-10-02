0ffe7e1044cb51c8c73159056e9c52410f92f23b
ThomasMelga <51729594+ThomasMelga@users.noreply.github.com>
2020-10-02 18:46:07 +0200

    Beregner nu resterende dage

--- src/main/java/com/example/demo/controller/MyController.java
+++ src/main/java/com/example/demo/controller/MyController.java
@@ -2,14 +2,21 @@ package com.example.demo.controller;
 
 import com.example.demo.model.Mad;
 import com.example.demo.service.DataStorage;
+import com.example.demo.service.ResterendeDage;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import java.time.temporal.ChronoUnit;
+
+import java.time.LocalDate;
+import java.util.Date;
 
 @Controller
 public class MyController {
+    ResterendeDage objRestDays = new ResterendeDage();
+
 
     @GetMapping("/")
     public String welcome() {
@@ -54,10 +61,29 @@ public class MyController {
     public String vedKnapTryk(Model model, @ModelAttribute("mad") Mad mad)
     {
         //Fylder arrayList med data fra textfields
-        DataStorage.tabelData(mad.getMadNavn(), mad.getAmount(), mad.getExpireDate());
+        DataStorage.tabelData(mad.getMadNavn(), mad.getAmount(), objRestDays.resterendeDage(mad.getExpireDate()));
+
+        //skal fange dato String
+        System.out.println("Dette er dato string: "+mad.getExpireDate());
+
+        //konvertere til LocalDate
+        //LocalDate expireDate = LocalDate.parse(mad.getExpireDate());
+        //System.out.println("Dette er dato LocalDate: " + expireDate);
+
+        //skal omregne dato til resterende dage
+        //long resterendeDage = ChronoUnit.DAYS.between(LocalDate.now(),expireDate );
+        //System.out.println("Antal dage imellem: " + resterendeDage);
+
+        //Skal konvertere fra long til String
+        //String resterendeDageString = String.valueOf(resterendeDage);
+        //skal indsætte resterende dage i tabel
+
+
 
         //Indsætter data i tabel fra ArrayList
         model.addAttribute("mad1", DataStorage.tabelData());
+
+
 
         return "reducer_dit_madspild";
     }
---
+++ src/main/java/com/example/demo/service/ResterendeDage.java
@@ -0,0 +1,32 @@
+package com.example.demo.service;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
+public class ResterendeDage {
+
+    /**
+     * Denne metode tager en string-dato "yyyy-mm-dd" og beregner antal dage fra dagsDato
+     * @param expireDate_
+     * @return
+     */
+    public String resterendeDage(String expireDate_)
+    {
+        //konvertere til LocalDate
+        LocalDate expireDate = LocalDate.parse(expireDate_);
+        System.out.println("Dette er dato LocalDate: " + expireDate);
+
+        //skal omregne dato til resterende dage
+        long resterendeDage = ChronoUnit.DAYS.between(LocalDate.now(),expireDate );
+        System.out.println("Antal dage imellem: " + resterendeDage);
+
+        //Skal konvertere fra long til String
+        String resterendeDageString = String.valueOf(resterendeDage);
+
+
+        return resterendeDageString;
+    }
+
+
+
+}
